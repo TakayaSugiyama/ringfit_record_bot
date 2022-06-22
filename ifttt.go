d1775bc1dfc19ca01a5375033b3010755569c0fa
takayasugiyama <s.takaya1027@gmail.com>
2022-06-23 07:42:26 +0900

    hello world

---
+++ ifttt.go
@@ -0,0 +1,26 @@
+package ringfit_record_bot
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+func GetIfttt(w http.ResponseWriter, r *http.Request) {
+	var data struct {
+		Url string `json:"url"`
+	}
+
+	fmt.Println(r.Body)
+	if err := json.NewDecoder(r.Body).Decode(&data); err != nil {
+		fmt.Fprint(w, "can not decode url")
+		return
+	}
+
+	if data.Url == "" {
+		fmt.Fprint(w, "url is empty value")
+		return
+	}
+	fmt.Println(data.Url)
+	fmt.Println("request is valid")
+}
